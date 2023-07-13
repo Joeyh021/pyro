3ce2b0a12380bee6b65cd827a5cf9e646de0a4d9
Joey <joey@joeyh.dev>
2023-07-13 08:30:07 +0000

    faff around with otel/tracing stuff that didnt work

--- pyromaniac/src/main.rs
+++ pyromaniac/src/main.rs
@@ -1,22 +1,31 @@
-use tracing::Level;
-use tracing_subscriber::fmt::format::FmtSpan;
 mod api;
 mod config;
 mod runner;
+use tracing_subscriber::prelude::*;
 
 #[tokio::main]
 async fn main() {
-    tracing::subscriber::set_global_default(
-        tracing_subscriber::fmt::fmt()
-            .with_span_events(FmtSpan::ACTIVE)
-            .pretty()
-            .with_max_level(Level::DEBUG)
-            .finish(),
-    )
-    .expect("Could not install tracing subscriber");
+    init_tracing();
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(api::app().into_make_service())
         .await
         .expect("Could not start server");
+
+    opentelemetry::global::shutdown_tracer_provider();
+}
+
+fn init_tracing() {
+    let tracer = opentelemetry_jaeger::new_agent_pipeline()
+        .with_endpoint("localhost:6831")
+        .with_service_name("pyromaniac")
+        .with_auto_split_batch(true)
+        .install_batch(opentelemetry::runtime::Tokio)
+        .expect("Could not init otel tracer");
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_opentelemetry::layer().with_tracer(tracer))
+        .init();
 }
